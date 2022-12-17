78715012dced38d23d69798eb9086440ca528d39
GioaniBraouni <giannisbraounis21@gmail.com>
2022-12-17 23:34:03 +0200

    Bug #8

--- vulnerabilities/csp/source/jsonp.php
+++ vulnerabilities/csp/source/jsonp.php
@@ -1,13 +1,14 @@
 <?php
 header("Content-Type: application/json; charset=UTF-8");
 
-if (array_key_exists ("callback", $_GET)) {
-	$callback = $_GET['callback'];
+if (array_key_exists("callback", $_GET)) {
+	// Sanitize the callback parameter to prevent XSS attacks
+	$callback = filter_var($_GET['callback'], FILTER_SANITIZE_STRING);
 } else {
 	return "";
 }
 
-$outp = array ("answer" => "15");
+$outp = array("answer" => "15");
 
-echo $callback . "(".json_encode($outp).")";
+echo $callback . "(" . json_encode($outp) . ")";
 ?>
