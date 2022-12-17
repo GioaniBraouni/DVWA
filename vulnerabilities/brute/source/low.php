a9d938d3b5c5a4c08e2d8c5d3bd217019719e5a1
GioaniBraouni <giannisbraounis21@gmail.com>
2022-12-17 14:10:11 +0200

    Bug #7

--- vulnerabilities/brute/source/low.php
+++ vulnerabilities/brute/source/low.php
@@ -2,15 +2,23 @@
 
 if( isset( $_GET[ 'Login' ] ) ) {
 	// Get username
-	$user = $_GET[ 'username' ];
+	$username = $_GET[ 'username' ];
 
 	// Get password
-	$pass = $_GET[ 'password' ];
-	$pass = md5( $pass );
+	$password = $_GET[ 'password' ];
+	$encryptedPassword = md5( $password );
 
-	// Check the database
-	$query  = "SELECT * FROM `users` WHERE user = '$user' AND password = '$pass';";
-	$result = mysqli_query($GLOBALS["___mysqli_ston"],  $query ) or die( '<pre>' . ((is_object($GLOBALS["___mysqli_ston"])) ? mysqli_error($GLOBALS["___mysqli_ston"]) : (($___mysqli_res = mysqli_connect_error()) ? $___mysqli_res : false)) . '</pre>' );
+	// Create a prepared statement
+	$statement = mysqli_prepare($GLOBALS["___mysqli_ston"], "SELECT * FROM `users` WHERE user = ? AND password = ?");
+
+	// Bind parameters to the statement
+	mysqli_stmt_bind_param($statement, "ss", $username, $encryptedPassword);
+
+	// Execute the statement
+	mysqli_stmt_execute($statement);
+
+	// Get the result
+	$result = mysqli_stmt_get_result($statement);
 
 	if( $result && mysqli_num_rows( $result ) == 1 ) {
 		// Get users details
@@ -18,7 +26,7 @@ if( isset( $_GET[ 'Login' ] ) ) {
 		$avatar = $row["avatar"];
 
 		// Login successful
-		$html .= "<p>Welcome to the password protected area {$user}</p>";
+		$html .= "<p>Welcome to the password protected area {$username}</p>";
 		$html .= "<img src=\"{$avatar}\" />";
 	}
 	else {
@@ -26,6 +34,8 @@ if( isset( $_GET[ 'Login' ] ) ) {
 		$html .= "<pre><br />Username and/or password incorrect.</pre>";
 	}
 
+	// Close the statement and connection
+	mysqli_stmt_close($statement);
 	((is_null($___mysqli_res = mysqli_close($GLOBALS["___mysqli_ston"]))) ? false : $___mysqli_res);
 }
 
