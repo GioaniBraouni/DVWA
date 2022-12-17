4da3b23067476a919df8913868d92b3099f77b57
GioaniBraouni <giannisbraounis21@gmail.com>
2022-12-17 11:16:08 +0200

    Bug #6

--- vulnerabilities/xss_s/source/high.php
+++ vulnerabilities/xss_s/source/high.php
@@ -5,14 +5,18 @@ if( isset( $_POST[ 'btnSign' ] ) ) {
 	$message = trim( $_POST[ 'mtxMessage' ] );
 	$name    = trim( $_POST[ 'txtName' ] );
 
-	// Sanitize message input
+	// Sanitize message input & name input
 	$message = strip_tags( addslashes( $message ) );
-	$message = ((isset($GLOBALS["___mysqli_ston"]) && is_object($GLOBALS["___mysqli_ston"])) ? mysqli_real_escape_string($GLOBALS["___mysqli_ston"],  $message ) : ((trigger_error("[MySQLConverterToo] Fix the mysql_escape_string() call! This code does not work.", E_USER_ERROR)) ? "" : ""));
-	$message = htmlspecialchars( $message );
-
-	// Sanitize name input
 	$name = preg_replace( '/<(.*)s(.*)c(.*)r(.*)i(.*)p(.*)t/i', '', $name );
-	$name = ((isset($GLOBALS["___mysqli_ston"]) && is_object($GLOBALS["___mysqli_ston"])) ? mysqli_real_escape_string($GLOBALS["___mysqli_ston"],  $name ) : ((trigger_error("[MySQLConverterToo] Fix the mysql_escape_string() call! This code does not work.", E_USER_ERROR)) ? "" : ""));
+
+	if (isset($GLOBALS["___mysqli_ston"]) && is_object($GLOBALS["___mysqli_ston"])) {
+		$message = mysqli_real_escape_string($GLOBALS["___mysqli_ston"],  $message );
+		$message = htmlspecialchars( $message );
+		$name = mysqli_real_escape_string($GLOBALS["___mysqli_ston"],  $name );
+	} else {
+		trigger_error("[MySQLConverterToo] Fix the mysql_escape_string() call! This code does not work.", E_USER_ERROR);
+		$message = $name = "";
+	}
 
 	// Update database
 	$query  = "INSERT INTO guestbook ( comment, name ) VALUES ( '$message', '$name' );";
