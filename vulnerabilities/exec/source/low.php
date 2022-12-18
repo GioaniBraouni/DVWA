d18ef19d793afb2703b4017e6a98690c7e90726e
GioaniBraouni <giannisbraounis21@gmail.com>
2022-12-18 23:39:01 +0200

    Revert "Bug #9"
    This reverts commit d3ddf8534da30b3fc6ded94fb2df7eae8c6c7100.

--- vulnerabilities/exec/source/low.php
+++ vulnerabilities/exec/source/low.php
@@ -1,35 +1,21 @@
 <?php
 
 if( isset( $_POST[ 'Submit' ]  ) ) {
-	/*
-	  Get Ip address from user.Also validate and sanitize to prevent command injection
-	  using function filter_input() with FILTER_VALIDATE_IP flag.
-	  Only correct formatted IP addresses are used for the ping command.
-	*/
-	$target = filter_input(INPUT_POST, 'ip', FILTER_VALIDATE_IP);
+	// Get input
+	$target = $_REQUEST[ 'ip' ];
 
-	//IP address is incorrect
-	if ($target === false) {
-		echo "Error: Invalid IP address.";
-		exit;
-	}
-	/*
-	  Determine OS and execute the ping command.
-	  Use escapeshellarg() function to escape the user-supplied IP
-	  address before passing it as an argument to the ping command to prevent command injection attack.
-	  The user input is treated as a string and not as part of the command itself.
-	*/
+	// Determine OS and execute the ping command.
 	if( stristr( php_uname( 's' ), 'Windows NT' ) ) {
 		// Windows
-		$cmd = shell_exec( 'ping ' . escapeshellarg($target) );
+		$cmd = shell_exec( 'ping  ' . $target );
 	}
 	else {
 		// *nix
-		$cmd = shell_exec( 'ping -c 4 ' . escapeshellarg($target) );
+		$cmd = shell_exec( 'ping  -c 4 ' . $target );
 	}
 
 	// Feedback for the end user
-	echo "<pre>{$cmd}</pre>";
+	$html .= "<pre>{$cmd}</pre>";
 }
 
 ?>
