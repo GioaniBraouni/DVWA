a28338ed560864e5ac6bde7d9cef647849eb5288
GioaniBraouni <giannisbraounis21@gmail.com>
2022-12-19 00:21:18 +0200

    Bug #9

--- vulnerabilities/exec/source/low.php
+++ vulnerabilities/exec/source/low.php
@@ -1,21 +1,37 @@
 <?php
 
 if( isset( $_POST[ 'Submit' ]  ) ) {
-	// Get input
-	$target = $_REQUEST[ 'ip' ];
+	/*
+	  Get Ip address from user.Also validate and sanitize to prevent command injection
+	  using function filter_input() with FILTER_VALIDATE_IP flag.
+	  Only correct formatted IP addresses are used for the ping command.
+	*/
+	$target = filter_input(INPUT_POST, 'ip', FILTER_VALIDATE_IP);
 
-	// Determine OS and execute the ping command.
+	//IP address is incorrect
+	if ($target === false) {
+		echo "Error: Invalid IP address.";
+		exit;
+	}
+	/*
+	  Determine OS and execute the ping command.
+	  Use escapeshellarg() function to escape the user-supplied IP
+	  address before passing it as an argument to the ping command to prevent command injection attack.
+	  The user input is treated as a string and not as part of the command itself.
+	*/
 	if( stristr( php_uname( 's' ), 'Windows NT' ) ) {
 		// Windows
-		$cmd = shell_exec( 'ping  ' . $target );
+		// 1st reference
+		$cmd = shell_exec( 'ping ' . escapeshellarg($target) );
 	}
 	else {
 		// *nix
+		// 2nd reference
 		$cmd = shell_exec( 'ping  -c 4 ' . $target );
 	}
 
 	// Feedback for the end user
-	$html .= "<pre>{$cmd}</pre>";
+	echo "<pre>{$cmd}</pre>";
 }
 
 ?>
